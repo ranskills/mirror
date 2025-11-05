1abe5983b8f36c514d71a208292d10b6e5b48b8d
Ransford Okpoti <ranskills@yahoo.co.uk>
2025-11-05 10:49:42 +0000

    remove commented out codes

--- app.py
+++ app.py
@@ -41,41 +41,9 @@ TELEGRAM_CHAT_ID = get_secret('TELEGRAM_CHAT_ID')
 telegram_client = create_client(TELEGRAM_TOKEN, TELEGRAM_CHAT_ID)
 
 
-# class LiveChatRequestInput(BaseModel):
-#   name: str = Field(description='The name of the user')
-
-
-# # print(pydantic.__version__)
-# @tool(args_schema=LiveChatRequestInput)
-# def live_chat_request_notifier(name: str):
-#   '''Detects if the user expresses the desire to chat/interact with me live'''
-#   r = f'Hello {name}, skills will now be responding to you live!'
-#   print(r)
-#   return r
-
-
-# @tool
-# def sent_telegram_message(message: str):
-#   '''In live mode, send the message to this channel'''
-
-#   chat_id=1358504684
-#   url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
-#   data = {'chat_id': chat_id, 'text': message}
-#   response = requests.post(url, json=data)
-#   print("Status Code:", response.status_code)
-#   print(response.json())
-
-# @tool
-# def capture_unanswered_question(question: str):
-#     '''Captures questions that are not answered'''
-#     print(f'I do not have an answer for the question: {question}')
 def ask_llm(message, history, state):
     pass
-    # print('Current State', state)
-    # messages = history + [{'role': 'user', 'content': message}]
-    # result = agent.invoke({'messages': messages})
 
-    # return result['messages'][-1].content
 
 def init_session() -> Session:
    global sessions
@@ -153,13 +121,7 @@ def chat(message, history, state: Session):
     history = state.history
 
     is_requesting_live_chat = message.lower() in ['mirror', 'mirror mirror']
-    # TODO: look for exit, e.g. exit, bye, bye
     should_exit_live_chat = message.lower() in ['exit', 'goodbye', 'bye', 'done', ]
-
-    # if not state.name:
-    #     state.name = message.strip()
-    #     response = f'Hello {state.name}, how can I assist you today?'
-    #     return '', history, state
 
     if is_requesting_live_chat:
         state.is_live_chat = True
@@ -174,56 +136,19 @@ def chat(message, history, state: Session):
     history.append({'role': 'user', 'content': message})
 
     if state.is_live_chat:
-        # if should_exit_live_chat:
-        #     state['is_live_chat'] = False
-
-        #     return '', 'Live chat ended, back to normal mode', state
-        # sent_telegram_message.invoke({'message': message})
         result = telegram_client.send_message(message)
         print(f'Sent result: {type(result)} {result}')
         message_id = result.get('message_id')
         print(f'Adding new message id: {message_id}')
         state.message_ids.add(message_id)
-        # sleep(2)
-        # telegram_updates = telegram_client.get_updates()
-        # print('Telegram Updates')
-        # print(telegram_updates)
-        # x = telegram_updates[-1]
-        # print('x' * 80)
-        # print(x)
-        # TODO: get response
-        # return '', x['message']['text'], state
+
         return '', history, state
 
 
     history.append({'role': 'assistant', 'content': response})
 
     return '', history, state
-    # live_chat_request = message.lower() == 'mirror mirror'
-    # # TODO: look for exit, e.g. exit, bye, bye
-    # exit_detected = message.lower() in ['exit', 'bye']
-    # if live_chat_request:
-    #     state['is_live_chat'] = True
-    # #   state['session_id'] = str(uuid.uuid4())
-    #     return 'Live chat **activated** 👤↔️👤, if he is not busy, you will get a response\nTo exit live chat mode, type "exit"', state
 
-    # if state['is_live_chat']:
-    #     if exit_detected:
-    #         state['is_live_chat'] = False
-
-    #         return 'Live chat ended, back to normal mode', state
-    #     # sent_telegram_message.invoke({'message': message})
-    #     telegram_client.send_message(message)
-    #     sleep(2)
-    #     telegram_updates = telegram_client.get_updates()
-    #     print('Telegram Updates')
-    #     print(telegram_updates)
-    #     x = telegram_updates[-1]
-    #     print('x' * 80)
-    #     print(x)
-    #     # TODO: get response
-    #     return x['message']['text'], state
-    # return ask_llm(message, history, state), state
 
 title = '🪞 Mirror'
 with gr.Blocks(title=title, fill_height=True) as ui:
